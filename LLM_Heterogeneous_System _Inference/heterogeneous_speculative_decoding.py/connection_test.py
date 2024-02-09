a37733ebd5d14cad42068b667c4cf7b2edc44db8
Yixiao Zeng <yixiaozeng0208@outlook.com>
2024-02-09 12:10:05 -0800

    02/09/2024 Update

--- LLM_Heterogeneous_System _Inference/heterogeneous_speculative_decoding.py/connection_test.py
+++ LLM_Heterogeneous_System _Inference/heterogeneous_speculative_decoding.py/connection_test.py
@@ -8,7 +8,7 @@ import torch
 def double_send(rank):
     if rank == 0: # server
         draft_output_shape = torch.empty((2),dtype=torch.long)
-        req = dist.irecv(tensor=draft_output_shape,src=1)
+        req = dist.recv(tensor=draft_output_shape,src=1)
         req.wait()
         print(f"draft_output_shape from src 1 should be [1,1] is {draft_output_shape}")
         # draft_output = torch.empty((draft_output_shape[0],draft_output_shape[1]),dtype=torch.long)
@@ -16,27 +16,36 @@ def double_send(rank):
         # req.wait()
     if rank ==1: # edge
         shape = torch.tensor([1,1])
-        req = dist.isend(tensor=shape,dst=0)
+        req = dist.send(tensor=shape,dst=0)
         req.wait()
 
 
-def init_processes(rank, size,IP, Port ,backend='nccl'):
+def init_processes(rank, size,IP, Port ,backend='gloo'):
     """ Initialize the distributed environment. """    
     print('{} : started process for rank : {}'.format(os.getpid(),rank))
 	
-    os.environ['MASTER_ADDR'] = "66.42.104.193"
-    os.environ['MASTER_PORT'] = '6100'
+    # os.environ['MASTER_ADDR'] = "66.42.104.193"
+    # os.environ['MASTER_PORT'] = '6100'
 	#Remove init_method if initializing through environment variable
     dist.init_process_group(backend = backend, 
-                            # init_method=f'tcp://{IP}:{Port}',
+                            init_method=f'tcp://{IP}:{Port}',
                             rank=rank,
                             world_size=size,
-                            timeout=datetime.timedelta(0,seconds =  20))
+                            timeout=datetime.timedelta(1,seconds =  20))
     #dist.init_process_group(backend, rank=rank, world_size=size)
 
 
 if __name__ == "__main__":
-    print(dist.is_available)
-    init_processes(0,2,'66.42.104.193','6100')
-    # init_processes(1,2,'192.168.0.44','1234')
-    double_send(rank=0)
+    print(dist.is_available())
+    print(dist.is_nccl_available())
+    # init_processes(0,2,'66.42.104.193','6100')
+    # # init_processes(1,2,'192.168.0.44','1234')
+    # double_send(rank=0)
+    # os.environ['MASTER_ADDR'] = '66.42.104.193'  # IP of Machine 1
+    # os.environ['MASTER_PORT'] = '6100'    
+    IP =  '66.42.104.193'   
+    # IP = "192.168.0.146"
+    Port = "6100"
+    dist.init_process_group(backend='nccl', init_method=f'tcp://{IP}:{Port}',rank=0, world_size=2,timeout=datetime.timedelta(1,seconds =  20))
+    print('connected')
+    double_send(0)
